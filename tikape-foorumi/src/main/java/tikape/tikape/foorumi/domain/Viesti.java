f5527b91352a4e68fe67eeb971b12fda561dd419
Toivonen Kim P <tokito@da5-cs-b221-12.helsinki.fi>
2016-03-02 15:33:07 +0200

    moi

---
+++ tikape-foorumi/src/main/java/tikape/tikape/foorumi/domain/Viesti.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.tikape.foorumi.domain;
+
+/**
+ *
+ * @author tokito
+ */
+public class Viesti {
+    
+}
