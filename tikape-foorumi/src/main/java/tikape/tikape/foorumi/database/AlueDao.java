88946ef48b70ffafe16ee84baa59c81e4f068564
Toivonen Kim P <tokito@da5-cs-bk107-27.helsinki.fi>
2016-03-03 16:05:06 +0200

    kyselyt aluedaoon

--- tikape-foorumi/src/main/java/tikape/tikape/foorumi/database/AlueDao.java
+++ tikape-foorumi/src/main/java/tikape/tikape/foorumi/database/AlueDao.java
@@ -1,23 +1,41 @@
-
 package tikape.tikape.foorumi.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import tikape.tikape.foorumi.domain.Alue;
 
-
-public class AlueDao extends AbstraktiDao<Alue, Integer>{
+public class AlueDao extends AbstraktiDao<Alue, Integer> {
 
     public AlueDao(Database db) {
         super(db, "Alue");
+    }
+
+    public int viestejaYhteensa(Alue a) throws Exception {
+        
+        int tulos = 0;
+        String id = a.getId() + "";
+
+        Connection c = this.db.getConnection();
+        PreparedStatement ps = c.prepareStatement("SELECT COUNT(viesti.id) AS id FROM viesti v, avaus a WHERE v.avaus = a.id AND a.alue = ?");
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            tulos = rs.getInt("id");
+        }
+        return tulos;
     }
 
     @Override
     public Alue createT(ResultSet rs) throws Exception {
         int id = rs.getInt("id");
         String nimi = rs.getNString("nimi");
-        
+
         Alue a = new Alue(id, nimi);
         return a;
     }
@@ -25,14 +43,14 @@ public class AlueDao extends AbstraktiDao<Alue, Integer>{
     @Override
     public List<String> values(Alue t) throws Exception {
         List<String> lista = new ArrayList();
-        
+
         String id = "" + t.getId();
         String nimi = t.getNimi();
-        
+
         lista.add(id);
         lista.add(nimi);
-        
+
         return lista;
     }
-    
+
 }
