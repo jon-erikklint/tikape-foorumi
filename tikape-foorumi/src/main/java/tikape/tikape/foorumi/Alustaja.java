ec94975ef5bed852a8ba0c19bb69ae898cd666e9
Isosaari Matias L J <imatias@da5-cs-bk107-28.helsinki.fi>
2016-03-03 16:50:54 +0200

    aluesivu get(ei toimi vielä)

--- tikape-foorumi/src/main/java/tikape/tikape/foorumi/Alustaja.java
+++ tikape-foorumi/src/main/java/tikape/tikape/foorumi/Alustaja.java
@@ -1,46 +1,65 @@
 package tikape.tikape.foorumi;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.*;
+import spark.ModelAndView;
 import tikape.tikape.foorumi.database.*;
 import spark.Spark.*;
+import static spark.Spark.get;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import tikape.tikape.foorumi.domain.Alue;
 
 public class Alustaja {
-    
+
     private AlueDao alueDao;
     private AvausDao avausDao;
     private Kayttajadao kayttajaDao;
     private ViestiDao viestiDao;
-    
-    public void alustaSql(){
+
+    public void alustaSql() {
         Database db = luoDatabase();
-        
+
         alueDao = new AlueDao(db);
         avausDao = new AvausDao(db);
         kayttajaDao = new Kayttajadao(db);
         viestiDao = new ViestiDao(db);
     }
-    
-    private Database luoDatabase(){
+
+    private Database luoDatabase() {
         String osoite = "jdbc:sqlite:foorumi.db";
-        
+
         return new Database(osoite);
     }
-    
-    public void alustaKuuntelijat(){
+
+    public void alustaKuuntelijat() {
         aihesivu();
         aluesivu();
         viestisivu();
     }
-    
-    private void aihesivu(){
-        
+
+    private void aihesivu() {
+
     }
-    
-    private void aluesivu(){
+
+    private void aluesivu() throws SQLException, ClassNotFoundException {
         
-    }
-    
-    private void viestisivu(){
+        get("/", (req, res) -> {
+            Map map = new HashMap<>();
         
+            List<Alue> lista = alueDao.findAll();
+            
+            map.put("Alueet", lista);
+
+            return new ModelAndView(map, "alueet");
+        },
+                new ThymeleafTemplateEngine());
     }
-    
+
+    private void viestisivu() {
+        
+
+    }
+
 }
--- tikape-foorumi/src/main/java/tikape/tikape/foorumi/Main.java
+++ tikape-foorumi/src/main/java/tikape/tikape/foorumi/Main.java
@@ -7,6 +7,7 @@ public class Main {
         
         ia.alustaSql();
         ia.alustaKuuntelijat();
+        
     }
     
 }
